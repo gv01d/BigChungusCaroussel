e08a38e40aa9bd47d0d5714a2f2b4f8385aa59b2
Gabriel Cunha Schlegel <78859340+gv01d@users.noreply.github.com>
2023-06-05 00:07:07 -0300

    Version 1

---
+++ Carousel.js
@@ -0,0 +1,90 @@
+// V1.0.0
+
+onload = () => {
+    //Button Left
+    let BtLeft = document.getElementById("left");
+    //Button Right
+    let BtRight = document.getElementById("right");
+    //Parent of cards
+    const caroussel = document.getElementById("caroussel");
+
+
+    //First Card
+    let Fst = document.querySelector("#caroussel>li:first-child");
+    //Last Card
+    let Lst = document.querySelector("#caroussel>li:last-child");
+
+    //Inicial Values
+    let Wait = false;
+    let Size = -1;
+    //Find Number of Cards
+    for (Size = 1; document.querySelector(`#caroussel>li:nth-child(${Size}n)`) != Lst; Size++);
+    //Main card (class "M<value>" - 0 middle, -1 left, +1 right,can go from -99 to 99)
+    let Main = parseInt(Size / 2) + (Size % 2) + parseInt(caroussel.className.substring(caroussel.className.lastIndexOf("M") + 1, caroussel.className.lastIndexOf("M-") + 3));
+    document.querySelector(`#caroussel>li:nth-child(${Main}n)`).className = "Main";
+
+    //Animation
+    const Cgo = (x) => {
+        return ([
+            { transform: `translateX(0px)` },
+            { transform: `translateX(${x}px)`, easing: "ease-out" }]);
+    };
+
+    //Animation Timing config
+    const CTiming = {
+        //Animations duration - Milisseconds
+        duration: 200,
+        iterations: 1,
+    };
+
+    //Button Left trigger
+    BtLeft.addEventListener("click", () => {
+        if (Wait) {
+            console.log("LeftButton Error : Please Wait");
+        }
+        else {
+            document.querySelector(`#caroussel>li:nth-child(${Main}n)`).className = " ";
+            Wait = true;
+            let Gap = parseInt(window.getComputedStyle(Fst.parentElement).gap);
+            console.log(Gap);
+            let Offset = Gap + (document.querySelector(`#caroussel>li:nth-child(${Main - 1}n)`).clientWidth);
+            console.log(Offset);
+            caroussel.animate(Cgo(Offset), CTiming).onfinish = () => {
+                let Lhtml = Lst.innerHTML;
+                for (let i = Size; i > 1; i--) {
+                    document.querySelector(`#caroussel>li:nth-child(${i}n)`).innerHTML = document.querySelector(`#caroussel>li:nth-child(${i - 1}n)`).innerHTML;
+                }
+                Fst.innerHTML = Lhtml;
+                document.querySelector(`#caroussel>li:nth-child(${Main}n)`).className = "Main";
+                Wait = false;
+            };
+        }
+    });
+
+    //Button Right trigger
+    BtRight.addEventListener("click", () => {
+        if (Wait) {
+            console.log("RightButton Error : Please Wait");
+        }
+        else {
+            Wait = true;
+            document.querySelector(`#caroussel>li:nth-child(${Main}n)`).className = " ";
+            let Gap = parseInt(window.getComputedStyle(Fst.parentElement).gap);
+            console.log(Gap);
+            let Offset = Gap + (document.querySelector(`#caroussel>li:nth-child(${Main + 1}n)`).clientWidth);
+            console.log(Offset);
+            caroussel.animate(Cgo(-Offset), CTiming).onfinish = () => {
+                let Lhtml = Fst.innerHTML;
+                for (let i = 1; i < Size; i++) {
+                    document.querySelector(`#caroussel>li:nth-child(${i}n)`).innerHTML = document.querySelector(`#caroussel>li:nth-child(${i + 1}n)`).innerHTML;
+                }
+                Lst.innerHTML = Lhtml;
+                document.querySelector(`#caroussel>li:nth-child(${Main}n)`).className = "Main";
+                Wait = false;
+            };
+        }
+    });
+
+
+
+}
---
+++ README.txt
@@ -0,0 +1,12 @@
+This Caroussel is highly customizable
+
+You can change the size of the LI's and add Card inside of it
+Use the class "Main" as an activation for your Card on focus
+Preferably dont change teh LI's, but you can add more
+
+Use the class on UL (LI's parent) to set the Activated card 
+(M0 for middle, M-x for left,Mx for right, x beeing any number between 0 and 99)
+example M-1,M0,M2,M-4
+
+You can also change the parent of all(div class="carosel" on this case) width and hight however you want
+
